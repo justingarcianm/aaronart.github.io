711fbcbee129f34df57cf0a479dbe8a025c93f4a
justin garcia <justinframedev@gmail.com>
2018-12-28 13:40:01 -0700

    add  comments to all pages and adjust media queries

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// Terrible and messy yet functional filter for gallery page
+
+// assign variables for filter bar and imgs
 var people = document.querySelector(".people");
 var animals = document.querySelector(".animals");
 var objects = document.querySelector(".objects");
@@ -5,29 +8,36 @@ var locations = document.querySelector(".locations");
 var all = document.querySelector(".all");
 var filterImg = document.querySelectorAll(".imgFilter");
 
+// assign variables for images
 var imgPeople = document.querySelectorAll(".row .people");
 var imgAnimals = document.querySelectorAll(".row .animals");
 var imgObjects = document.querySelectorAll(".row .objects");
 var imgLocations = document.querySelectorAll(".row .locations");
 
 
-
+//  events for clicking on filter bar
 people.addEventListener("click", peopleFilter, false);
 animals.addEventListener("click", animalFilter, false);
 objects.addEventListener("click", objectFilter, false);
 locations.addEventListener("click", locationFilter, false);
 all.addEventListener("click", allFilter, false);
 
+// All the functions for the filter bar
+// Hides all images other than selected filter
 function peopleFilter() {
     filterImg.forEach(function () {
+        // remove active class from all buttons
+        // add active class to selected button
         all.classList.remove("active");
         animals.classList.remove("active");
         objects.classList.remove("active");
         locations.classList.remove("active");
         people.classList.add("active");
+        // for each loop that hides all the images
         for (var i = 0; i < filterImg.length; i++) {
             filterImg[i].style.display = "none";
             imgPeople.forEach(function () {
+                // for each loop that shows all of the selected filter
                 for (var i = 0; i < imgPeople.length; i++) {
                     imgPeople[i].style.display = 'block';
                 }
@@ -35,6 +45,7 @@ function peopleFilter() {
         }
     })
 }
+// rinse and repeat for the rest
 function animalFilter() {
     filterImg.forEach(function () {
         all.classList.remove("active");
@@ -86,6 +97,7 @@ function locationFilter() {
         }
     })
 }
+// this simply resets the gallery to its initial state
 function allFilter() {
     all.classList.add("active");
     animals.classList.remove("active");
@@ -98,3 +110,5 @@ function allFilter() {
         }
     })
 }
+
+// Although this is messy and not DRY qualified, my goal was to simple get it to work and in the future I will improve on the code
